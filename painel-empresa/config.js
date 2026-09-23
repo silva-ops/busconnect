cfe1ad30272599ed96814fecf607fd96c8b04409
silva-ops <serujo@gmail.com>
2026-09-23 11:37:40 -0300

    Configura URL do backend no Render para os 3 paineis

--- painel-empresa/config.js
+++ painel-empresa/config.js
@@ -1,4 +1,2 @@
 // Configuracao do backend para producao
-// APOS fazer o deploy no Render, substitua o valor abaixo pela URL real.
-// Exemplo: 'https://busconnect-backend.onrender.com'
-window.BUSCONNECT_BACKEND_URL = '';
+window.BUSCONNECT_BACKEND_URL = 'https://busconnect-backend-gykj.onrender.com';
