20189efad903db905f27c2cd131b82d4abd9028e
silva-ops <serujo@gmail.com>
2026-09-25 18:49:06 -0300

    Limpa repositorio: remove scripts auxiliares e arquivos sensiveis

--- app-passageiro/corrigir-icone.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const arq = 'C:/Users/SSA/Desktop/busconnect/app-passageiro/index.html';
-let txt = fs.readFileSync(arq, 'utf8');
-txt = txt.replace(
-  /<div class="icone-grande">[\s\S]*?<\/div>/,
-  '<div class="icone-grande">&#9888;&#65039;</div>'
-);
-fs.writeFileSync(arq, txt, 'utf8');
-console.log('OK icone corrigido.');
